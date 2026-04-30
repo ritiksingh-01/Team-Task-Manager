b7515f555dd51a64c1bb61ce595dcadd5e27d7e2
Ritik Singh <144662342+Ritiksingh02@users.noreply.github.com>
2026-05-01 01:25:39 +0530

    fix: bind server to 0.0.0.0 for Railway deployment

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ const startServer = (port) => {
     throw error;
   });
 
-  server.listen(port, () => {
+  server.listen(port, '0.0.0.0', () => {
     console.log(`Server running on port ${port}`);
   });
 };
